6294d18d5c0a943e6155380eb8a14ae09d8d4778
Roykssop <herna14@gmail.com>
2016-10-31 00:52:24 -0300

    Node module socket io

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ server.port = 3000;
 server.start();
 
 server.app.use('/api/questions/', require('./src/routes/questions')(server));
+
+var a = require(__dirname + '/src/lib/sockio')(server);
+
 server.on('mess', function(){
     console.log("Esto es un evento lindo");
 })
