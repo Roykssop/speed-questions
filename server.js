e29e9392ca3dbe38ff7f01fca3972697d7a9d3ce
Roykssop <herna14@gmail.com>
2016-11-05 15:12:21 -0300

    Added main page

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var server = function(){
         self.instance = self.app.listen(self.port, function(){
                                 console.log('running');
                             })
-        self.sockio =  require('socket.io')(self.instance);
+        self.sockio =  require('socket.io')(self.instance); // Socket io takes server instance
     }
 
     self.app.use(express.static('public'));
