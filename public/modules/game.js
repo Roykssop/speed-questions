c0c7688ced0a12b8ad839b1599ea3e00df63216a
Roykssop <herna14@gmail.com>
2016-11-12 14:17:04 -0300

    commit

--- public/modules/game.js
+++ public/modules/game.js
@@ -1,18 +1,22 @@
-var module = angular.module('gameManager',[]);
+var module = angular.module('gameManager',['room']);
 
-module.service('game', function(){
+module.service('game', function($room){
     var self = this;
     var maxPlayers = 4;
     var maxQuestions = 10;
     var roomName = "";
     var players = [];
     var rooms = [];
+    var roomi;
 
     self.start = function(oConstruct){
-
+        var roomi = $room.getInstance();
     }
 
-    
+    self.addRoom = function(oRoom){
+        roomi.create(oRoom);
+        
+    }    
 
 
 
--- public/modules/room.js
+++ public/modules/room.js
@@ -1,6 +1,6 @@
     angular
         .module('room',[players])
-        .factory('room', Factory)
+        .factory('$room', Factory)
 
     /** @ngInject */
     function Factory($q,$http){
@@ -17,17 +17,41 @@
                     status = true
                 else
                     status = false;
+
+                return status
             }
 
             function loadQuestions(){
 
             }
 
+            function setMaxPlayers(_maxPlayers){
+                maxPlayers = _maxPlayers;
+            }
+
+            function setName(_name){
+                name = _name;
+            }
+
+            function setMaxQuestions(_maxQuestions){
+                maxQuestions = _maxQuestions;
+            }            
+
             return {
                 addPlayer : function(){},
                 removePlayer : function(){},
-                startRoom : function(){},
-                countPlayers : function{} 
+                create : function(oNewRoom){
+                    if (!oNewRoom.name || !oNewRoom.maxPlayers || !oNewRoom.maxQuestions )
+                        return false;
+
+                    setMaxPlayers(oNewRoom.maxPlayers);
+                    setName(oNewRoom.name);
+                    setMaxQuestions(oNewRoom.maxQuestions);
+                },
+                countPlayers : function(){},
+                statusRoom : function(){
+                    return checkStatus();
+                } 
             }
 
         }
